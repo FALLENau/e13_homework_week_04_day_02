21754a20c793fc55942bf3a224a4c48f30d9a7ef
FALLENau <reece.c.jones@gmail.com>
2017-06-13 18:56:39 +0100

    homework

--- start/pizza_controller.rb
+++ start/pizza_controller.rb
@@ -13,6 +13,11 @@ end
 get '/pizzas/new' do
   erb(:new)
 end
+# EDIT
+get "/pizzas/:id/edit" do
+  @pizza = Pizza.find(params["id"].to_i)
+  erb(:edit)
+end
 
 # SHOW
 get '/pizzas/:id' do
@@ -22,6 +27,21 @@ end
 
 # CREATE
 post "/pizzas" do
-  # binding.pry
-  @pizzas = Pizza.save()
+  @pizza = Pizza.new(params)
+  @pizza.save()
+  erb(:create)
+end
+
+# UPDATE
+post "/pizzas/:id/update" do
+  @pizza = Pizza.new(params)
+  @pizza.update()
+  erb(:update)
+end
+
+# DELETE
+post "/pizzas/:id/delete" do
+  @pizza = Pizza.find(params["id"].to_i())
+  @pizza.delete()
+  erb(:delete)
 end
