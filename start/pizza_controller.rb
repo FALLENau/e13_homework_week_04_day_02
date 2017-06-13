fdc52a0b671371ef87787b17a253b3a8151d5b70
FALLENau <reece.c.jones@gmail.com>
2017-06-13 15:45:29 +0100

    mind blowngit add .

--- start/pizza_controller.rb
+++ start/pizza_controller.rb
@@ -1,3 +1,27 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require( 'pry-byebug' )
+require_relative( 'models/pizza' )
+
+# INDEX
+get '/pizzas' do
+  @pizzas = Pizza.all()
+  erb(:index)
+end
+
+# NEW
+get '/pizzas/new' do
+  erb(:new)
+end
+
+# SHOW
+get '/pizzas/:id' do
+  @pizza = Pizza.find(params["id"].to_i())
+  erb(:show)
+end
+
+# CREATE
+post "/pizzas" do
+  # binding.pry
+  @pizzas = Pizza.save()
+end
